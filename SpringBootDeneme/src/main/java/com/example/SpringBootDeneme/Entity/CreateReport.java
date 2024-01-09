e111ad2329dc0ace32bdf8cc99f58084904f05f0
Yakup Özdemir <yakupozdemir2003@hotmail.com>
2024-01-09 23:38:35 +0300

    wip

--- SpringBootDeneme/src/main/java/com/example/SpringBootDeneme/Entity/CreateReport.java
+++ SpringBootDeneme/src/main/java/com/example/SpringBootDeneme/Entity/CreateReport.java
@@ -1,24 +1,32 @@
 package com.example.SpringBootDeneme.Entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 @Entity
 @Data
 public class CreateReport {
 
     @Id
-
     private String imgName;
-
     @Lob
-    private byte[] data;
-
+    private byte[] imageData;
     @OneToOne
     private Reports reports;
-
     @OneToOne
     private Patients patients;
 
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+    public CreateReport() {
+        super();
+    }
 
+    public CreateReport(String imgName, byte[] imageData, Reports reports, Patients patients) {
+        this.imgName = imgName;
+        this.imageData = imageData;
+        this.reports = reports;
+        this.patients = patients;
+    }
 }
